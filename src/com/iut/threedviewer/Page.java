6f6f8ddddb14a3735112fe5fe05230331a4711b4
florian <florian.barrois25@orange.fr>
2014-03-30 21:12:11 +0200

    ça marche pas putin

--- src/com/iut/threedviewer/Page.java
+++ src/com/iut/threedviewer/Page.java
@@ -11,28 +11,28 @@ import android.widget.TextView;
 
 public class Page extends Activity {
 
-	Button button;
-	ImageView image;
+	Button button = new Button(null);
+	ImageView image = new ImageView(null);
  
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.x_wing);
  
-		addListenerOnButton();
+		addListenerOnButton(button = (Button) findViewById(R.id.button1));
  
 	}
  
-	public void addListenerOnButton() {
+	public void addListenerOnButton(Button button) {
  
 		image = (ImageView) findViewById(R.id.imageView1);
  
-		button = (Button) findViewById(R.id.button1);
+//		button = (Button) findViewById(R.id.button1);
 		button.setOnClickListener(new OnClickListener() {
  
 			@Override
 			public void onClick(View arg0) {
-				image.setImageResource(R.drawable.tie);
+				image.setImageResource(R.drawable.bunny);
 			}
  
 		});
